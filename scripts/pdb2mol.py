c84e6346eaa451463f9a8db170b5b2439703ca53
Aleksandra <avnikonenko.volg@gmail.com>
2023-11-01 09:12:01 +0100

    fix charge of O- carboxylic acid

--- scripts/pdb2mol.py
+++ scripts/pdb2mol.py
@@ -6,29 +6,40 @@ import os
 import re
 import logging
 import datetime
+import tempfile
+import subprocess
 
 from rdkit import Chem
 from rdkit.Chem import *
 
-def fix_valence_charge(m):
+def fix_valence_charge(m, mol_name):
     m.UpdatePropertyCache(strict=False)
     ps = Chem.DetectChemistryProblems(m)
     for p in ps:
         if p.GetType() != 'KekulizeException':
             at = m.GetAtomWithIdx(p.GetAtomIdx())
+
+            print(f'Warning!!!!!!! Your reference tautomeric form doesnot match with your pdb. {mol_name}. '
+                  f'Atom - ID:{at.GetIdx()} Atomic Num:{at.GetAtomicNum()}. {p.GetType()}')
+
             if at.GetAtomicNum() == 7 and at.GetTotalValence() == 4 and at.GetFormalCharge() != 1:
                 at.SetFormalCharge(1)
 
             if at.GetAtomicNum() == 5 and at.GetTotalValence() == 4 and at.GetFormalCharge() != -1:
                 at.SetFormalCharge(-1)
-
-            if at.GetNumImplicitHs() > 0:
-                at.SetFormalCharge(at.GetFormalCharge() - at.GetNumImplicitHs())
-                at.SetNoImplicit(True)
     return m
 
+def update_charge(mol, mol_name):
+    for a in mol.GetAtoms():
+        if a.GetNumImplicitHs() != 0 :
+            print(f'Warning!!!!!!! Your reference tautomeric form doesnot match with your pdb. {mol_name}. '
+                  f'Atom - ID:{a.GetIdx()} Atomic Num:{a.GetAtomicNum()}')
+            a.SetFormalCharge(a.GetFormalCharge()-a.GetNumImplicitHs())
+            a.SetNoImplicit(True)
 
-def AssignBondOrdersFromTemplate_fix(refmol, mol):
+    return mol
+
+def AssignBondOrdersFromTemplate_fix(refmol, mol, mol_name):
   refmol2 = rdchem.Mol(refmol)
   mol2 = rdchem.Mol(mol)
   # do the molecules match already?
@@ -72,7 +83,9 @@ def AssignBondOrdersFromTemplate_fix(refmol, mol):
 
       mol2 = Chem.RemoveHs(mol2, implicitOnly=True)
       Chem.KekulizeIfPossible(mol2)
-      mol2 = fix_valence_charge(mol2)
+      mol2 = fix_valence_charge(mol2, mol_name)
+
+      mol2 = update_charge(mol2, mol_name)
 
       status = Chem.SanitizeMol(mol2, catchErrors=True)
       if status is not Chem.rdmolops.SanitizeFlags.SANITIZE_NONE:
@@ -83,18 +96,46 @@ def AssignBondOrdersFromTemplate_fix(refmol, mol):
       return None, 'no matching found'
   return mol2, []
 
-def convertpdb2mol(input_fnames, input_smi, regex):
+
+def get_major_microspecies(smi_fname, h='7.4', tautomerize=True):
+    canon_smi_dict = {}
+    output = os.path.join(os.path.dirname(smi_fname), f'{os.path.basename(smi_fname).split(".")[0]}'
+                                                       f'_protonation{"_majortautomer" if tautomerize else ""}'
+                                                       f'_pH{h}.smi')
+    cmd_run = ['cxcalc', '-S', '--ignore-error', 'majormicrospecies', '-H', h,
+               f'{"-M" if tautomerize else ""}', smi_fname]
+    with open(output, 'w') as file:
+        subprocess.run(cmd_run, stdout=file, text=True)
+    for mol in Chem.SDMolSupplier(output, sanitize=False):
+        if mol:
+            smi = mol.GetPropsAsDict().get('MAJORMS', None)
+            mol_name = mol.GetProp('_Name') if mol.HasProp('_Name') else smi
+            if smi is not None:
+                try:
+                    cansmi = Chem.CanonSmiles(smi)
+                    canon_smi_dict[mol_name.lower()] = cansmi
+                except:
+                    sys.stderr.write(f'ERROR: {mol_name}, Reference smiles {smi} obtained after protonation '
+                                     f'could not be read by RDKit. The molecule was skipped.\n')
+
+    return canon_smi_dict
+
+def convertpdb2mol(input_fnames, input_smi, regex, no_protonation, no_tautomerization):
 
     smis = dict()
     if (input_smi is not None) and (os.path.isfile(input_smi)) and (input_smi.lower().endswith('.smi') or input_smi.lower().endswith('.smiles')):
-        with open(input_smi) as f:
-            for line in f:
-                values = line.strip().split()
-                if len(values) >= 2:
-                    smis[values[1].lower()] = values[0]
-                else:
-                    sys.stderr.write(
-                        f'Line "{line}" in input smiles does not have two fields - SMILES and mol name. Skipped.\n')
+        if no_protonation:
+            with open(input_smi) as f:
+                for line in f:
+                    values = line.strip().split()
+                    if len(values) >= 2:
+                        smis[values[1].lower()] = values[0]
+                    else:
+                        sys.stderr.write(
+                            f'Line "{line}" in input smiles does not have two fields - SMILES and mol name. Skipped.\n')
+        else:
+            smis = get_major_microspecies(input_smi, tautomerize=not no_tautomerization)
+
 
     for in_fname in input_fnames:
         print(in_fname)
@@ -117,8 +158,10 @@ def convertpdb2mol(input_fnames, input_smi, regex):
 
         mol = Chem.MolFromPDBFile(in_fname, sanitize=False, removeHs=False)
         template = Chem.MolFromSmiles(smi)
-        mol_new, ps = AssignBondOrdersFromTemplate_fix(template, mol)
+        mol_new, ps = AssignBondOrdersFromTemplate_fix(template, mol, mol_name)
         if not ps:
+            # to make all hydrogens explicit
+            mol_new = Chem.AddHs(mol_new, addCoords=True)
             mol_new.SetProp('_Name', mol_name)
             Chem.MolToMolFile(mol_new, in_fname.replace('.pdb', '.mol'))
             print(in_fname+' finished successfully')
@@ -143,9 +186,14 @@ def main():
                              'Use regex search to establish a relationship between reference smiles name and pdbqt '
                              'filename. If None filename of pdbqt file will be taken to find reference smiles '
                              'Examples: MOLID[0-9]* or .*(?=_out.pdbqt)')
+    parser.add_argument('--no_protonation', action='store_true', default=False,
+                        help='disable protonation of molecules before docking. Protonation requires installed '
+                             'cxcalc chemaxon utility.')
+    parser.add_argument('--no_tautomerization', action='store_true', default=False,
+                        help='disable tautomerization of molecules during protonation.')
 
     args = parser.parse_args()
-    convertpdb2mol(input_fnames=args.input, input_smi=args.smi, regex=args.regex)
+    convertpdb2mol(input_fnames=args.input, input_smi=args.smi, regex=args.regex, no_protonation=args.no_protonation, no_tautomerization=args.no_tautomerization)
 
 
 
